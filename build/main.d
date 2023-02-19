4e05f4a8840298ee74cb2b73aaf87d2c1ac79321
xXHighTideXx <84487860+TheHighTide@users.noreply.github.com>
2023-02-19 13:56:52 -0800

    Add files via upload

---
+++ build/main.d
@@ -0,0 +1,179 @@
+main.o: C:/Users/henry/Documents/FalloutTerminal/source/main.c \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/types.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/result.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/ipc.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/svc.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/exheader.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/srv.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/errf.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/os.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/synchronization.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/thread.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/gfx.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/gspgpu.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/console.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/env.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/util/decompress.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/util/utf.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/allocator/linear.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/allocator/mappable.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/allocator/vram.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/ac.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/am.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/fs.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/ampxi.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/apt.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/boss.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/cam.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/y2r.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/cfgnor.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/cfgu.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/csnd.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/dsp.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/fspxi.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/fsreg.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/frd.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/mii.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/gsplcd.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/hid.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/irrst.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/sslc.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/httpc.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/uds.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/ndm.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/nim.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/nwmext.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/ir.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/ns.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/pmapp.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/pmdbg.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/ps.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/ptmu.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/ptmsysm.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/ptmgets.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/ptmsets.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/pxidev.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/pxipm.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/soc.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/netinet/in.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/sys/socket.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/mic.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/mvd.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/nfc.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/news.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/qtm.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/srvpm.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/loader.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/mcuhwc.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/cdcchk.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/gpu/gx.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/gpu/gpu.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/gpu/registers.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/gpu/enums.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/gpu/shbin.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/gpu/shaderProgram.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/ndsp/ndsp.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/ndsp/channel.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/applets/swkbd.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/applets/error.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/applets/miiselector.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/archive.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/romfs.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/font.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/gdbhio_dev.h \
+ C:/Users/henry/Documents/devkitpro/libctru/include/3ds/3dslink.h \
+ C:/Users/henry/Documents/FalloutTerminal/include/clear_animations.h \
+ C:/Users/henry/Documents/FalloutTerminal/include/animations.h \
+ C:/Users/henry/Documents/FalloutTerminal/include/anim_hidden.h \
+ C:/Users/henry/Documents/FalloutTerminal/include/terminal_menu.h \
+ C:/Users/henry/Documents/FalloutTerminal/include/terminal_text.h
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/types.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/result.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/ipc.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/svc.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/exheader.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/srv.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/errf.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/os.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/synchronization.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/thread.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/gfx.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/gspgpu.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/console.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/env.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/util/decompress.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/util/utf.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/allocator/linear.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/allocator/mappable.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/allocator/vram.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/ac.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/am.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/fs.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/ampxi.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/apt.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/boss.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/cam.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/y2r.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/cfgnor.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/cfgu.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/csnd.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/dsp.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/fspxi.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/fsreg.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/frd.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/mii.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/gsplcd.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/hid.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/irrst.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/sslc.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/httpc.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/uds.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/ndm.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/nim.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/nwmext.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/ir.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/ns.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/pmapp.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/pmdbg.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/ps.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/ptmu.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/ptmsysm.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/ptmgets.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/ptmsets.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/pxidev.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/pxipm.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/soc.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/netinet/in.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/sys/socket.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/mic.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/mvd.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/nfc.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/news.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/qtm.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/srvpm.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/loader.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/mcuhwc.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/services/cdcchk.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/gpu/gx.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/gpu/gpu.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/gpu/registers.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/gpu/enums.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/gpu/shbin.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/gpu/shaderProgram.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/ndsp/ndsp.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/ndsp/channel.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/applets/swkbd.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/applets/error.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/applets/miiselector.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/archive.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/romfs.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/font.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/gdbhio_dev.h:
+C:/Users/henry/Documents/devkitpro/libctru/include/3ds/3dslink.h:
+C:/Users/henry/Documents/FalloutTerminal/include/clear_animations.h:
+C:/Users/henry/Documents/FalloutTerminal/include/animations.h:
+C:/Users/henry/Documents/FalloutTerminal/include/anim_hidden.h:
+C:/Users/henry/Documents/FalloutTerminal/include/terminal_menu.h:
+C:/Users/henry/Documents/FalloutTerminal/include/terminal_text.h:
---
+++ source/main.c
@@ -0,0 +1,373 @@
+#include <3ds.h>
+#include <stdio.h>
+#include <string.h>
+
+// Animations:
+#include <clear_animations.h>
+#include <animations.h>
+#include <anim_hidden.h>
+
+// Terminal Features:
+#include <terminal_menu.h>
+#include <terminal_text.h>
+
+static SwkbdCallbackResult MyCallback(void* user, const char** ppMessage, const char* text, size_t textlen)
+{
+	if (strstr(text, "War never changes"))
+	{
+		*ppMessage = "No it doesn't :(\n\n\nSadly";
+		return SWKBD_CALLBACK_CONTINUE;
+	}
+
+	if (strstr(text, "I don't enjoy killing but when done rightousley, it's just another chore"))
+	{
+		*ppMessage = "Killing is not good either way! DON'T KILL PEOPLE!!!!!!";
+		return SWKBD_CALLBACK_CLOSE;
+	}
+
+	if (strstr(text, "Waging war against good people is bad for the soul"))
+	{
+		*ppMessage = "It is... It really is";
+		return SWKBD_CALLBACK_CLOSE;
+	}
+
+	if (strstr(text, "And don't get me started on the futility of dusting a collapsed house!"))
+	{
+		*ppMessage = "Now doing that won't be that smart (it would take a hella long time). Why did you even bring this up in the first place?";
+		return SWKBD_CALLBACK_CLOSE;
+	}
+
+	if (strstr(text, "If you believe in something enough, you must be willing to let it burn, lest it claim you"))
+	{
+		*ppMessage = "Ok and that is the most badass thing I have ever heard anyone say ever!\n\nEven more badass than the doom slayer saying 'rip and tear, until it is done'!";
+		return SWKBD_CALLBACK_CLOSE;
+	}
+
+	return SWKBD_CALLBACK_OK;
+}
+
+int main(int argc, char **argv)
+{
+	// Settings
+	int clearAnimationType = 1;
+	int clearAnimationSpeed = 5;
+
+	int selectedSetting = 1;
+
+	bool isBackgroundDark = true;
+
+	// Checks
+	bool isNotePadOpen = false;
+	bool isTypingAnEmoji = false;
+	bool isInSettings = false;
+
+	// Keys
+	char keysNames[32][32] = {
+		"KEY_A", "KEY_B", "KEY_SELECT", "KEY_START",
+		"KEY_DRIGHT", "KEY_DLEFT", "KEY_DUP", "KEY_DDOWN",
+		"KEY_R", "KEY_L", "KEY_X", "KEY_Y",
+		"", "", "KEY_ZL", "KEY_ZR",
+		"", "", "", "",
+		"KEY_TOUCH", "", "", "",
+		"KEY_CSTICK_RIGHT", "KEY_CSTICK_LEFT", "KEY_CSTICK_UP", "KEY_CSTICK_DOWN",
+		"KEY_CPAD_RIGHT", "KEY_CPAD_LEFT", "KEY_CPAD_UP", "KEY_CPAD_DOWN"
+	};
+
+	// Main Code
+	gfxInitDefault();
+	PrintConsole topScreen, bottomScreen;
+	consoleInit(GFX_TOP, &topScreen);
+	consoleInit(GFX_BOTTOM, &bottomScreen);
+
+	consoleSelect(&topScreen);
+	PlayAnimation(1);
+	printf("\033[32mTerminal Loading.\033[0m\n");
+	svcSleepThread(2500000000);
+	printf("\033[32mTerminal Loaded!\033[0m\n");
+	svcSleepThread(1000000000);
+	consoleClear();
+
+	consoleSelect(&bottomScreen);
+	DisplayTerminalMenu(1, false, 0);
+
+	while (aptMainLoop())
+	{
+		hidScanInput();
+		u32 kDown = hidKeysDown();
+
+		if (isNotePadOpen == true){
+			static SwkbdState swkbd;
+			static char mybuf[200];
+			static SwkbdStatusData swkbdStatus;
+			static SwkbdLearningData swkbdLearning;
+			SwkbdButton button = SWKBD_BUTTON_NONE;
+			bool didit = false;
+
+			if (kDown & KEY_DUP)
+			{
+				didit = true;
+				swkbdInit(&swkbd, SWKBD_TYPE_NORMAL, 3, -1);
+				swkbdSetInitialText(&swkbd, mybuf);
+				swkbdSetHintText(&swkbd, "Your text here...");
+				swkbdSetButton(&swkbd, SWKBD_BUTTON_LEFT, "Cancel", false);
+				swkbdSetButton(&swkbd, SWKBD_BUTTON_MIDDLE, "Exit", true);
+				swkbdSetButton(&swkbd, SWKBD_BUTTON_RIGHT, "Continue", true);
+				swkbdSetFeatures(&swkbd, SWKBD_PREDICTIVE_INPUT);
+				SwkbdDictWord words[2];
+				swkbdSetDictWord(&words[0], "lenny", "( ͡° ͜ʖ ͡°)");
+				swkbdSetDictWord(&words[1], "shrug", "¯\\_(ツ)_/¯");
+				swkbdSetDictionary(&swkbd, words, sizeof(words)/sizeof(SwkbdDictWord));
+				static bool reload = false;
+				swkbdSetStatusData(&swkbd, &swkbdStatus, reload, true);
+				swkbdSetLearningData(&swkbd, &swkbdLearning, reload, true);
+				reload = true;
+				button = swkbdInputText(&swkbd, mybuf, sizeof(mybuf));
+			}
+
+			if (kDown & KEY_DDOWN){
+				consoleClear();
+			}
+
+			if (kDown & KEY_A){
+				consoleSelect(&bottomScreen);
+				consoleClear();
+				DisplayTerminalMenu(2, true, 0);
+				consoleSelect(&topScreen);
+				DisplayTerminalText(3);
+				consoleSelect(&bottomScreen);
+				consoleClear();
+				DisplayTerminalMenu(2, false, 0);
+			}
+
+			if (kDown & KEY_START){
+				isNotePadOpen = false;
+				consoleClear();
+				consoleSelect(&bottomScreen);
+				consoleClear();
+				DisplayTerminalMenu(1, false, 0);
+			}
+
+			if (didit)
+			{
+				if (button != SWKBD_BUTTON_NONE){
+					printf("\x1b[32m");
+					printf(mybuf);
+					printf("\x1b[0m\n");
+				}
+			}
+		}
+		else if (isInSettings == true){
+			consoleSelect(&bottomScreen);
+			consoleClear();
+			// First Checks
+			if (clearAnimationSpeed == 1){
+				if (clearAnimationType == 1){
+					DisplayTerminalMenu(3, false, 1);
+				}
+				else if (clearAnimationType == 2){
+					DisplayTerminalMenu(3, false, 11);
+				}
+			}
+			else if (clearAnimationSpeed == 2){
+				if (clearAnimationType == 1){
+					DisplayTerminalMenu(3, false, 2);
+				}
+				else if (clearAnimationType == 2){
+					DisplayTerminalMenu(3, false, 12);
+				}
+			}
+			else if (clearAnimationSpeed == 3){
+				if (clearAnimationType == 1){
+					DisplayTerminalMenu(3, false, 3);
+				}
+				else if (clearAnimationType == 2){
+					DisplayTerminalMenu(3, false, 13);
+				}
+			}
+			else if (clearAnimationSpeed == 4){
+				if (clearAnimationType == 1){
+					DisplayTerminalMenu(3, false, 4);
+				}
+				else if (clearAnimationType == 2){
+					DisplayTerminalMenu(3, false, 14);
+				}
+			}
+			else if (clearAnimationSpeed == 5){
+				if (clearAnimationType == 1){
+					DisplayTerminalMenu(3, false, 5);
+				}
+				else if (clearAnimationType == 2){
+					DisplayTerminalMenu(3, false, 15);
+				}
+			}
+			else if (clearAnimationSpeed == 6){
+				if (clearAnimationType == 1){
+					DisplayTerminalMenu(3, false, 6);
+				}
+				else if (clearAnimationType == 2){
+					DisplayTerminalMenu(3, false, 16);
+				}
+			}
+			else if (clearAnimationSpeed == 7){
+				if (clearAnimationType == 1){
+					DisplayTerminalMenu(3, false, 7);
+				}
+				else if (clearAnimationType == 2){
+					DisplayTerminalMenu(3, false, 17);
+				}
+			}
+			else if (clearAnimationSpeed == 8){
+				if (clearAnimationType == 1){
+					DisplayTerminalMenu(3, false, 8);
+				}
+				else if (clearAnimationType == 9){
+					DisplayTerminalMenu(3, false, 18);
+				}
+			}
+			else if (clearAnimationSpeed == 9){
+				if (clearAnimationType == 1){
+					DisplayTerminalMenu(3, false, 9);
+				}
+				else if (clearAnimationType == 2){
+					DisplayTerminalMenu(3, false, 19);
+				}
+			}
+			else if (clearAnimationSpeed == 10){
+				if (clearAnimationType == 1){
+					DisplayTerminalMenu(3, false, 10);
+				}
+				else if (clearAnimationType == 2){
+					DisplayTerminalMenu(3, false, 20);
+				}
+			}
+
+			// Main
+			if (selectedSetting == 1){
+				printf("\x1b[9;30H\033[32m ");
+				printf("\x1b[8;30H\033[32m<");
+			}
+			if (selectedSetting == 2){
+				printf("\x1b[8;30H\033[32m ");
+				printf("\x1b[9;30H\033[32m<");
+			}
+			if (kDown & KEY_DDOWN){
+				if (selectedSetting == 1){
+					selectedSetting = 2;
+				}
+				else if (selectedSetting == 2){
+					selectedSetting = 1;
+				}
+			}
+			else if (kDown & KEY_DRIGHT){
+				if (selectedSetting == 1){
+					if (clearAnimationSpeed == 10){
+						clearAnimationSpeed = 10;
+					}
+					else {
+						clearAnimationSpeed = clearAnimationSpeed + 1;
+					}
+				}
+				else if (selectedSetting == 2){
+					if (clearAnimationType == 2){
+						clearAnimationType = 1;
+					}
+					else if (clearAnimationType == 1){
+						clearAnimationType = 2;
+					}
+				}
+			}
+			else if (kDown & KEY_DLEFT){
+				if (selectedSetting == 1){
+					if (clearAnimationSpeed == 1){
+						clearAnimationSpeed = 1;
+					}
+					else {
+						clearAnimationSpeed = clearAnimationSpeed - 1;
+					}
+				}
+				else if (selectedSetting == 2){
+					if (clearAnimationType == 2){
+						clearAnimationType = 1;
+					}
+					else if (clearAnimationType == 1){
+						clearAnimationType = 2;
+					}
+				}
+			}
+			else if (kDown & KEY_START){
+				isInSettings = false;
+				consoleClear();
+				DisplayTerminalMenu(1, false, 0);
+			}
+		}
+		else if (isNotePadOpen == false & isInSettings == false){
+			if (kDown & KEY_START) break;
+			else if (kDown & KEY_A){
+				consoleSelect(&bottomScreen);
+				consoleClear();
+				DisplayTerminalMenu(1, true, 0);
+				consoleSelect(&topScreen);
+				consoleClear();
+				DisplayTerminalText(1);
+				consoleSelect(&bottomScreen);
+				consoleClear();
+				DisplayTerminalMenu(1, false, 0);
+			}
+			else if (kDown & KEY_B){
+				consoleSelect(&bottomScreen);
+				consoleClear();
+				DisplayTerminalMenu(1, true, 0);
+				consoleSelect(&topScreen);
+				consoleClear();
+				DisplayTerminalText(2);
+				consoleSelect(&bottomScreen);
+				consoleClear();
+				DisplayTerminalMenu(1, false, 0);
+			}
+			else if (kDown & KEY_X){
+				consoleSelect(&bottomScreen);
+				consoleClear();
+				DisplayTerminalMenu(2, false, 0);
+				consoleSelect(&topScreen);
+				consoleClear();
+				isNotePadOpen = true;
+			}
+			else if (kDown & KEY_Y){
+				consoleSelect(&bottomScreen);
+				consoleClear();
+				DisplayTerminalMenu(3, false, 0);
+				consoleSelect(&topScreen);
+				consoleClear();
+				isInSettings = true;
+			}
+			else if (kDown & KEY_L){
+				consoleSelect(&bottomScreen);
+				consoleClear();
+				DisplayTerminalMenu(1, true, 0);
+				consoleSelect(&topScreen);
+				consoleClear();
+				AnimationHidden(0);
+				consoleSelect(&bottomScreen);
+				consoleClear();
+				DisplayTerminalMenu(1, false, 0);
+			}
+			else if (kDown & KEY_SELECT){
+				consoleSelect(&bottomScreen);
+				consoleClear();
+				DisplayTerminalMenu(1, true, 0);
+				consoleSelect(&topScreen);
+				consoleClearAnimation(clearAnimationType, clearAnimationSpeed * 1000);
+				consoleClear();
+				consoleSelect(&bottomScreen);
+				consoleClear();
+				DisplayTerminalMenu(1, false, 0);
+			}
+		}
+
+		gfxFlushBuffers();
+		gfxSwapBuffers();
+		gspWaitForVBlank();
+	}
+	gfxExit();
+	return 0;
+}
